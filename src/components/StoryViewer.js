45f5a7972d33014d5b971b0246c13be39563e463
Hebron George <hebrontgeorge@gmail.com>
2020-05-10 14:01:16 -0500

    Remove is-4by3 class from image

--- src/components/StoryViewer.js
+++ src/components/StoryViewer.js
@@ -13,7 +13,7 @@ export default function StoryViewer(props) {
     <div className="tile is-ancestor">
       <div className="tile is-6 is-parent">
 
-        <figure className="image is-4by3">
+        <figure className="image">
           <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder">
           </img>
         </figure>
